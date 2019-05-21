3a3827fe8185650f42d5ba445445e5d38fa13e10
rabite <rabite@posteo.de>
2019-05-21 23:29:57 +0200

    flush input before reading

--- src/widget.rs
+++ src/widget.rs
@@ -554,10 +554,10 @@ fn event_thread(rx_global: Receiver<Events>,
 fn input_thread(tx: Sender<Events>, rx_input_request: Receiver<()>) {
     std::thread::spawn(move || {
         for input in stdin().events() {
-            term::flush_stdin();
             input.map(|input| {
                 tx.send(Events::InputEvent(input)).unwrap();
                 rx_input_request.recv().unwrap();
+                term::flush_stdin();
             }).map_err(|e| HError::from(e)).log();
         }
     });
