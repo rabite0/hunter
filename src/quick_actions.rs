b18b2bb9ad7b3f245401bd1f9da4b3ff195fe820
rabite <rabite@posteo.de>
2019-05-28 01:14:53 +0200

    add quick_actions.rs

--- src/quick_actions.rs
+++ src/quick_actions.rs
@@ -68,11 +68,12 @@ impl FoldableWidgetExt for ListView<Vec<QuickActions>> {
     fn on_key(&mut self, key: Key) -> HResult<()> {
         match key {
             Key::Char('a') |
-            Key::Char('h') => HError::popup_finnished()?,
+            Key::Char('b') => HError::popup_finnished()?,
             // undefined key causes parent to handle move up/down
-            Key::Char('j') => HError::undefined_key(key)?,
-            Key::Char('k') => HError::undefined_key(key)?,
-            Key::Char('l') => self.run_action(None),
+            Key::Char('n') => HError::undefined_key(key)?,
+            Key::Char('p') => HError::undefined_key(key)?,
+            Key::Char('f') => self.run_action(None),
+
             key @ Key::Char(_) => {
                 let chr = match key {
                     Key::Char(key) => key,
@@ -192,7 +193,7 @@ impl ListView<Vec<QuickActions>> {
 }
 
 // shouldn't contain keys used for navigation/activation
-static CHARS: &str = "bcdefgimoqrstuvxyz";
+static CHARS: &str = "bcdeghijklmoqrstuvxyz";
 
 impl QuickActions {
     pub fn new(files: Vec<File>,
