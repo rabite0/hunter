1210f0dce8cbd55af30866457312933cde3c3061
ath3 <ha05190@protonmail.com>
2019-07-03 01:33:58 +0200

    Moved the ratio logic to HBox, column ratios are not based on 0-99 anymore, improved parsing from config file

--- src/config.rs
+++ src/config.rs
@@ -138,26 +138,14 @@ impl Config {
                 },
                 Ok(("ratios", ratios)) => {
                     let ratios_str = ratios.to_string();
-                    let mut ratios: Vec<usize> = ratios_str.split([',', ':'].as_ref())
-                        .map(|r| r.trim().parse::<usize>().unwrap()).collect();
-                    if ratios.len() == 3 {
-                        let mut ratios_sum: usize = ratios.iter().sum();
-                        if ratios_sum != 99 {
-                            if ratios_sum != 100 {
-                                ratios = ratios.iter().map(|&r| (r as f64 * 100.0 / ratios_sum as f64)
-                                    .round() as usize).collect();
-                                ratios_sum = ratios.iter().sum();
-                            }
-                            while ratios_sum > 99 {
-                                let ratios_max = ratios.iter()
-                                    .position(|&r| r == *ratios.iter().max().unwrap()).unwrap();
-                                ratios[ratios_max] = ratios[ratios_max] - 1;
-                                ratios_sum -= 1;
-                            }
-                        }
-                        let ratios_min = ratios.iter().min().unwrap();
-                        if *ratios_min > 2 {
-                            config.ratios = ratios;
+                    if ratios_str.chars().all(|x| x.is_digit(10) || x.is_whitespace()
+                        || x == ':' || x == ',' ) {
+                            let ratios: Vec<usize> = ratios_str.split([',', ':'].as_ref())
+                                .map(|r| r.trim().parse::<usize>().unwrap()).collect();
+                            let ratios_sum: usize = ratios.iter().sum();
+                            if ratios.len() == 3 && ratios_sum > 0 && ratios.iter()
+                                .filter(|&r| *r > u16::max_value() as usize).next() == None {
+                                    config.ratios = ratios;
                         }
                     }
                 }
